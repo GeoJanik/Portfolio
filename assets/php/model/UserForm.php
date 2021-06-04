e96dae52253390f03acb4ce388dec818130a998f
Geo Janik <geo.janik@gmail.com>
2021-06-04 09:55:27 +0200

    form admin ok

--- assets/php/model/UserForm.php
+++ assets/php/model/UserForm.php
@@ -1,8 +1,6 @@
 <?php
 
 namespace portfolio\model;
-
-
 class UserForm extends Manager
 {
     public function insertForm($firstName, $secondName, $email, $objectForm, $comment)
@@ -13,14 +11,18 @@ class UserForm extends Manager
         return $form;
     }
 
-    public function selectForm() {
+    public function selectForm()
+    {
         $db = $this->dbConnect();
         $valueForm = $db->prepare('SELECT id, firstName, secondName, email, objectForm, comment, DATE_FORMAT(dateForm, \'%d/%m/%Y à %Hh%imin%ss\') AS dateFormFr FROM form ORDER BY dateForm DESC ');
         $valueForm->execute();
         return $valueForm;
-
     }
 
-    
+    public function deleteForm($id)
+    {
+        $db = $this->dbConnect();
+        $deleteValues = $db->prepare('DELETE FROM form WHERE id = ?');
+        $deleteValues->execute(array($id));
+    }
 }
-
