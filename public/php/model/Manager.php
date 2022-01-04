044e4fc24ae3b62f792145c5e2ac473d58e3e723
unknown <geojanik@gmail.com>
2022-01-04 17:46:42 +0100

    mdp

--- public/php/model/Manager.php
+++ public/php/model/Manager.php
@@ -7,7 +7,7 @@ class Manager
 {
     protected function dbConnect()
     {
-        $db = new \PDO('mysql:host=db5002854922.hosting-data.io;dbname=dbs2285527;charset=utf8', 'dbu1921394', 'portfolioGeoJanik');
+        $db = new \PDO('mysql:host=db5002854922.hosting-data.io;dbname=dbs2285527;charset=utf8', 'dbu1921394', 'PortfolioGeo!@');
         return $db;
     }
 }
